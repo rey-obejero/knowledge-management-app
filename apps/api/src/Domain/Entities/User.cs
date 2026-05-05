bf13aec08aeb3ca9f5daafc2edad73708b921b87
Rey Obejero <rey.obejero.mail@gmail.com>
2026-05-06 07:57:19 +0800

    chore(api): delete unused User entity in Domain
    Delete unused User entity in Domain layer. User entities are defined as
    Identity Users in Infrastructure.

--- apps/api/src/Domain/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-namespace KnowledgeManagementApp.Api.Domain.Entities;
-
-public class User
-{
-    public Guid Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string UserName { get; set; } = string.Empty;
-}
